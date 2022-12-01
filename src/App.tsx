92609a172e33cb169d7949c97bdc93e2182727ae
benoitbelloc <benoit.belloc@ynov.com>
2022-12-01 11:29:05 +0100

    navbar sans navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './views/Home';
+import Navbar from './views/Navbar/Navbar';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
