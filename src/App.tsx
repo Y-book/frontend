0bfbc5a53473ad3d49159e9daedf4453d157b45a
benoitbelloc <benoit.belloc@ynov.com>
2022-12-15 10:45:46 +0100

    debut des messages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './views/Navbar/Navbar';
 import Home from './views/home/Home';
+import ListMessage from './views/message/ListMessage';
+import Message from './views/message/Message';
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/listMessage" element={<ListMessage />} />
+          <Route path="/message" element={<Message />} />
         </Routes>
       </header>
     </div>
