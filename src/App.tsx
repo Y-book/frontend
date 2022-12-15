219d647815311ecbc8d7fbc35fa7d382f4d72242
laura398 <lauramathieu.git@gmail.com>
2022-12-15 11:28:46 +0100

    Correction suppress messages

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import Home from './views/home/Home';
 import Login from './views/login/Login';
 import Signup from './views/signup/Signup';
 import SignupCode from './views/signup/Signup-code';
-import ListMessage from './views/message/ListMessage';
-import Message from './views/message/Message';
 
 function App() {
 
@@ -19,8 +17,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/code" element={<SignupCode />} />
-          <Route path="/listMessage" element={<ListMessage />} />
-          <Route path="/message" element={<Message />} />
         </Routes>
       </header>
     </div>
